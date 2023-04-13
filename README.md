adceeb1d8f167b81e1bf96690f3dc863e34a8419
Coss111 <124317426+Coss111@users.noreply.github.com>
2023-04-13 11:35:23 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-scp37b
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-ah2jk5)
---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img
+      alt="Vue logo"
+      src="https://imagenes.20minutos.es/files/og_thumbnail/uploads/imagenes/2021/04/22/el-perro-picado-por-una-abeja.jpeg"
+    />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
